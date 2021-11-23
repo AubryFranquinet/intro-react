c3aed2678053dd367a57b376650b3dca7a0639fc
Aubry Franquinet <aubry.franquinet@hotmail.com>
2021-11-23 11:01:05 +0100

    day 2

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,12 +4,21 @@ import { v4 as uuidv4 } from 'uuid';
 export default function Todo() {
     const initialTodos = ["My first todo", "My second todo"];
     const [todos, setTodos] = useState(initialTodos);
+    const [checked, setChecked] = useState(false);
+    
+    const handleChange = (e) => {
+
+        setChecked(!checked);
+        console.log(checked);
+        console.log(e);
+    };
+
     return (
         <div className="todoList">
          <h3>Todo List</h3>
             {todos.map((todo) => (
                 <div key={uuidv4()}>
-                    <input type="checkbox" /> 
+                    <input type="checkbox" onChange={handleChange} checked={checked}/>
                     {todo}
                 </div>
             ))}
@@ -18,6 +27,8 @@ export default function Todo() {
     )
     
 }
+
+
 
 
 
